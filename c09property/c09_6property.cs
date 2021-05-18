7384a091de8b9be5c454355f2c55915caae1f8fb
Woosangje <dnedrbs3@naver.com>
2021-05-18 21:23:47 +0900

    05/18 09property2

--- c09property/c09_6property.cs
+++ c09property/c09_6property.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace property09
+namespace property09_06
 {
     //인터페이스의 프로퍼티
-    /*인터페이스는 메소드뿐만 아니라 프로퍼티와 인덱서도 가질 수 있습니다.
-     파생 클래스는 부모 인터페이스에 선언된 모든 프로퍼티를 구현해야합니다.
-     */
-   interface INamedValue
+    //인터페이스는 메소드뿐만 아니라 프로퍼티와 인덱서도 가질 수 있습니다.
+     //프로퍼티나 인덱서를 가진 인터페이스를 상속하는 클래스는 반드시 해당 프로퍼티와 인덱서를 구현해야 합니다.
+     //파생 클래스는 부모 인터페이스에 선언된 모든 프로퍼티를 구현해야합니다.
+     
+   /*
+    interface INamedValue
     {
         string Name
         {
+            //자동 구현 프로퍼티처럼 구현이 없지만, C#컴파일러는 인터페이스의 프로퍼티에 대해서는 자동으로 구현해
+             //주지 않습니다. 이유는 인터페이스는 어떤 구현도 가지지 않기 때문입니다.
             get;
             set;
         }
@@ -24,6 +28,8 @@ namespace property09
 
     class NamedValue : INamedValue
     {
+        //INamedValue 인터페이스를 상속하는 NamedValue 클래스는 반드시 Name과
+        // Value를 구현해야 합니다. 이 때 자동 구현 프로퍼티를 이용하는 것도 가능합니다.
         public string Name
         {
             get;
@@ -35,7 +41,6 @@ namespace property09
             get;
             set;
         }
-
     }
 
     class MainApp
@@ -44,12 +49,12 @@ namespace property09
         {
             NamedValue name = new NamedValue()
             {
-                Name = "이름",
+                Name ="이름",
                 Value = "박상현"
             };
             NamedValue height = new NamedValue()
             {
-                Name ="키", Value = "177Cm"
+                Name ="키", Value ="177Cm"
             };
 
             NamedValue weight = new NamedValue()
@@ -63,4 +68,5 @@ namespace property09
             Console.WriteLine("{0} : {1}", weight.Name, weight.Value);
         }
     }
+   */
 }
---
+++ c09property/c09_7property.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace property09_7
+{
+    /*9.7추상 클래스와 프로퍼티*/
+    /*추상 클래스는 클래스처럼 구현된 프로퍼티를 가질 수도 있는 한편, 
+     * 인터페이스처럼 구현되지 않은 프로퍼티고 가질 수 있습니다.
+     추상 메소드가 그랬던 것처럼, 추상 프로퍼티 역시 인터페이스의 프로퍼티와 다를 것이없습니다.
+    파생 클래스가 해당 프로퍼티를 구현하도록 강제하는 것일 뿐이지요.
+
+    추상 클래스의 추상프로퍼티는 인터페이스처럼 구현을 비워놓은 것만으로는 추상 프로퍼티를 만들 수 없습니다.
+    C# 컴파일러가 자동 구현 프로퍼티로 간주하고 구현을 자동으로 채워넣을 테니까요.
+    추상 프로퍼티는 다음과 같이 abstract 한정자를 이용해서 선정합니다.
+     */
+
+   /* abstract class Product
+    {
+        private static int serial = 0;
+        public string SerialID
+        {
+            get { return String.Format("{0:d5}", serial++); }
+        }
+
+        abstract public DateTime ProductDate
+        {
+            get;
+            set;
+        }
+    }
+        class MyProduct : Product
+        {
+            public override DateTime ProductDate
+            {
+                get;
+                set;
+            }
+        }
+
+        class MainApp
+        {
+            static void Main(string[] args)
+            {
+                Product product_1 = new MyProduct()
+                { ProductDate = new DateTime(2014, 1, 10) };
+
+                Console.WriteLine("Product : {0}, Product Date : {1}",
+                        product_1.SerialID,
+                        product_1.ProductDate);
+
+                Product product_2 = new MyProduct()
+                {
+                    ProductDate = new DateTime(2014, 2, 3)
+                };
+
+                Console.WriteLine("Product :{0}, Product Date : {1}",
+                    product_2.SerialID,
+                    product_2.ProductDate);
+            }
+        }
+
+    */
+}
