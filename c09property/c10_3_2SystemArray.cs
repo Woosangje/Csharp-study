c80750ecc8c54f0cb678c9b3ac9a4a219776d920
Woosangje <dnedrbs3@naver.com>
2021-05-22 19:46:21 +0900

    5/22 array & Sellecter

--- c09property/c10_3_2SystemArray.cs
+++ c09property/c10_3_2SystemArray.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace property09
 {
-    class MainApp
+    /*class MainApp
     {
         private static bool CheckPassed(int score)
         {
@@ -14,7 +14,6 @@ namespace property09
                 return false;
 
         }
-
         private static void Print(int value)
         {
             Console.Write("{0} ", value);
@@ -28,7 +27,7 @@ namespace property09
                 Console.Write("{0} ", score);
             Console.WriteLine();
 
-            Array.Sort(scores);//작은값부터 높은값으로 정렬해줌
+            Array.Sort(scores);
             Array.ForEach<int>(scores, new Action<int>(Print));
             Console.WriteLine();
 
@@ -43,30 +42,34 @@ namespace property09
                 Array.TrueForAll<int>(scores, CheckPassed));
             //CheckPassed <- TrueForAll 메소드는 배열과 함께 조건을 검사하는 메소드를 함께 매개 변수로 받습니다.
 
-            int index = Array.FindIndex<int>(scores, 
-            delegate (int scores)
-            {
-                if (scores < 60)
-                    return true;
-                else
-                    return false;
-            });
+            int index = Array.FindIndex<int>(scores,
+                delegate (int scores)
+                {
+                    if (scores < 60)
+                        return true;
+                    else
+                        return false;
+                });
             scores[index] = 61;
+            Console.WriteLine("Everyone passed ? : {0}",
+                Array.TrueForAll<int>(scores, CheckPassed));
+
             Console.WriteLine("Everyone passed ? :{0}",
                 Array.TrueForAll<int>(scores, CheckPassed));
 
             Console.WriteLine("Old length of scores : {0}", scores.GetLength(0));
 
-            Array.Resize<int>(ref scores, 10);//5였던 배열의 용량을 10으로 재조정합니다.
+            Array.Resize<int>(ref scores, 10); //5였던 배열의 용량을 10으로 재종정합니다.
 
-            Console.WriteLine("New length of scores : {0}", scores.Length);
+            Console.WriteLine("New length of scores :{0}", scores.Length);
 
-            Array.ForEach<int>(scores, new Action<int>(Print));//Action 델리게이트에 대해서는 14장에서 자세하게 설명하겠습니다.
+            Array.ForEach<int>(scores, new Action<int>(Print));
             Console.WriteLine();
             Array.Clear(scores, 3, 7);
 
             Array.ForEach<int>(scores, new Action<int>(Print));
             Console.WriteLine();
+
         }
-    }
+    }*/
 }
---
+++ c09property/c10_7_2Queue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace UsingQueue
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Queue que = new Queue();
+            que.Enqueue(1);
+            que.Enqueue(2);
+            que.Enqueue(3);
+            que.Enqueue(4);
+            que.Enqueue(5);
+            que.Dequeue();
+            while (que.Count > 0)
+            {
+                Console.WriteLine(que.Dequeue());
+            }
+        }
+    }   
+}
