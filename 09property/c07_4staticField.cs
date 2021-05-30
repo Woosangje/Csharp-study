28812cc1c62ea3dfbbe3935f9382bceac7dbf352
Woosangje <dnedrbs3@naver.com>
2021-05-30 19:52:56 +0900

    this()

---
+++ 09property/c07_4staticField.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace property09
+{
+    /*class Global
+    {
+        public static int Count = 0;
+    }
+
+    class ClassA
+    {
+        public ClassA()
+        {
+            Global.Count++;
+        }
+    }
+
+    class ClassB
+    {
+        public ClassB()
+        {
+            Global.Count++;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main()
+        {
+            Console.WriteLine("Global.Count : {0}", Global.Count);
+
+            new ClassA();
+            new ClassA();
+            new ClassA();
+            new ClassB();
+
+            Console.WriteLine("Global.Count : {0}", Global.Count);
+        }
+    }*/
+}
