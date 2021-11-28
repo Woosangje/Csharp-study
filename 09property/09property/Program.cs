2ae2a1600ffdd87b695482e4d3ee8fbbcfdf6707
woosangje <dnedrbs3@naver.com>
2021-11-28 20:32:57 +0900

    1128b

--- 09property/09property/Program.cs
+++ 09property/09property/Program.cs
@@ -6,7 +6,22 @@ namespace _09property
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = 20;
+            Console.WriteLine("Type: {0}, Value: {1}", a.GetType(), a);
+
+            var b = 3.1414213;
+            Console.WriteLine("Type: {0}, Value: {1}", b.GetType(), b);
+
+            var c = "Hello, Would!";
+            Console.WriteLine("Type: {0}, Value: {1}", c.GetType(), c);
+
+            var d = new int[] { 10, 20, 30 };
+            Console.Write("Type: {0}, Value: {1}", c.GetType(), c);
+
+            foreach (var e in d)
+                Console.Write("{0} ", e);
+
+            Console.WriteLine();
         }
     }
 }
