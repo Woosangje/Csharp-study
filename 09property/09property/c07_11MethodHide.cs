231db7ddf958898b551173ce2233c7390ddf3b8c
woosangje <dnedrbs3@naver.com>
2021-12-01 21:18:05 +0900

    1201a

---
+++ 09property/09property/c07_11MethodHide.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c07_11MethodHide
+{ 
+   /* class Base
+    {
+        public void MyMethod()
+        {
+          
+                Console.WriteLine("Base.MyMethod()");
+            
+        }
+    }
+
+    class Derived : Base
+    {
+        public new void MyMethod()
+        {
+            Console.WriteLine("Derived.MyMethod()");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Base baseObj = new Base();
+            baseObj.MyMethod();
+
+            Derived derivedObj = new Derived();
+
+            derivedObj.MyMethod();
+
+            Base baseOrDerived = new Derived();
+            baseOrDerived.MyMethod();
+        }
+    }*/
+}
---
+++ 09property/09property/c07_12MethodSealed.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c07_12MethodSealed
+{
+    /*
+    class Base
+    {
+        public virtual void SealMe()
+        {
+
+        }
+    }
+
+    class Derived : Base
+    {
+        public sealed override void SealMe()
+        {
+
+        }
+    }
+
+    class WantToOverride : Derived
+    {
+        public override void SealMe()
+        {
+
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    */
+}
