037f49f090ce422771de761e18e4d65506db4c25
woosangje <dnedrbs3@naver.com>
2021-12-05 21:18:51 +0900

    1205a

--- 09property/09property/Program.cs
+++ 09property/09property/Program.cs
@@ -2,22 +2,5 @@
 
 namespace Program
 {
-    /*
 
-    abstract class human
-    {
-        private st
-    }
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            var nameCard = 0;
-            Console.WriteLine("이름:{0}, 나이:{1}", nameCard.Name, nameCard.Age);
-
-            var complex = "";
-            Console.WriteLine("Real:{0}, Imaginary:{1}",
-                complex.Real, complex.Imaginary);
-        }
-    }*/
 }
