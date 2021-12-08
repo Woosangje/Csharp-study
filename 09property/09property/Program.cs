1ceacdd5e5efb1731b72705f2c0407b6060f4245
woosangje <dnedrbs3@naver.com>
2021-12-08 21:01:11 +0900

    1208a

--- 09property/09property/Program.cs
+++ 09property/09property/Program.cs
@@ -3,20 +3,6 @@ using System.Collections.Generic;
 using System.Collections;
 
 namespace Program
-{/*
-   class MainApp
-    {
-        static void Main(string[] args)
-        {
-            try
-            {
-                int a = 1;
-                Console.WriteLine(3 / -a);
-            }
-            catch (DivideByZeroException e)
-            {
-                Console.WriteLine(e.StackTrace);
-            }
-        }
-    }*/
+{
+    
 }
