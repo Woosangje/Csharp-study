31a3d36de0db29128e809ffbfd39fa4669ed73f3
woosangje <dnedrbs3@naver.com>
2021-12-12 21:30:09 +0900

    1212a

--- 09property/09property/Program.cs
+++ 09property/09property/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections;
-
+using System.Linq;
 namespace Program
 {
-
-
+    
 }
---
+++ 09property/09property/c14_3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace c14_3
+{/*
+    class MainApp
+    {
+        delegate string Concatenate(string[] args);
+
+        static void Main(string[] args)
+        {
+            Concatenate concat =
+                (arr) =>
+                {
+                    string result = "1234";
+                    foreach (string s in arr)
+                        result += s;
+
+                    return result;
+                };
+            Console.WriteLine(concat(args));
+        }
+    }*/
+}
