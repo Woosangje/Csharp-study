891da41cadb01a9510ca5d59c26f779c75287303
woosangje <dnedrbs3@naver.com>
2021-12-16 20:59:50 +0900

    1216a

--- 09property/09property/c16_1_3Reflection.cs
+++ 09property/09property/c16_1_3Reflection.cs
@@ -3,32 +3,24 @@ using System.Reflection;
 using System.Reflection.Emit;
 
 namespace c16_1_3Reflection
-{/*
-    public class MainApp
+{//https://morm.tistory.com/227 코드오류떠서 수정
+   /* public class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-           
-                AssemblyBuilder newAssembly =
-                    AppDomain.CurrentDomain.DefineDynamicAssembly(
-                        new AssemblyName("CalculatorAssembly"),
-                        AssemblyBuilderAccess.Run);
+            AssemblyBuilder newAssembly = AssemblyBuilder.DefineDynamicAssembly(
+                new AssemblyName("CalculatorAssembly"), AssemblyBuilderAccess.Run);
 
-            ModuleBuilder newModule = newAssembly.DefineDynamicModule(
-                "Calculator");
+            ModuleBuilder newModule = newAssembly.DefineDynamicModule("Calculator");
             TypeBuilder newType = newModule.DefineType("Sum1To100");
 
-            MethodBuilder newMethod = newType.DefineMethod(
-                "Calculate",
-                MethodAttributes.Public,
-                typeof(int),
-                new Type[0]);
+            MethodBuilder newMethod = newType.DefineMethod("Calculate", MethodAttributes.Public, typeof(int), new Type[0]);
 
             ILGenerator generator = newMethod.GetILGenerator();
 
             generator.Emit(OpCodes.Ldc_I4, 1);
 
-            for(int i=2; i <= 100; i++)
+            for(int i =2; i <= 100; i++)
             {
                 generator.Emit(OpCodes.Ldc_I4, i);
                 generator.Emit(OpCodes.Add);
@@ -38,13 +30,9 @@ namespace c16_1_3Reflection
             newType.CreateType();
 
             object sum1To100 = Activator.CreateInstance(newType);
-            MethodInfo Calculate = sum1To100.GetType().
-                GetMethod("Calculate");
+
+            MethodInfo Calculate = sum1To100.GetType().GetMethod("Calculate");
             Console.WriteLine(Calculate.Invoke(sum1To100, null));
-
-
-
-            
         }
     }*/
 }
---
+++ 09property/09property/c16_2_1Basic_Attribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace c16_1_3Reflection
+{/*
+    class MyClass
+    {
+        
+        [Obsolete("OldMethod는 폐기되었습니다. NewMethod()를 이용하세요.")]
+        public void OldMethod()
+        {
+            Console.WriteLine("I'm old");
+        }
+
+        public void NewMethod()
+        {
+            Console.WriteLine("I'm new");
+        }
+
+        class MainApp
+        {
+            static void Main(string[] args)
+            {
+                MyClass obj = new MyClass();
+
+                obj.OldMethod();
+                obj.NewMethod();
+            }
+        }
+    }*/
+}
