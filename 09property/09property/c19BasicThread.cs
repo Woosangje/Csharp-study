8be721374d6dc95f87bcaabc3fa043c53ba99b37
woosangje <dnedrbs3@naver.com>
2022-01-09 22:03:28 +0900

    0109a

---
+++ 09property/09property/c19BasicThread.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+namespace c18_5Serialization
+{
+    /*
+    class MainApp
+    {
+
+        static void DoSomething()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("DoSomething : {0}", i);
+                Thread.Sleep(10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(DoSomething));
+
+            Console.WriteLine("Starting thread...");
+            t1.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Main : {0}", i);
+                Thread.Sleep(10);
+            }
+
+            Console.WriteLine("Wating untile thread stops...");
+            t1.Join();
+
+            Console.WriteLine("Finished");
+        }
+    }
+    */ 
+}
---
+++ 09property/09property/c19_2ThreadState.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+namespace c19_2ThreadState
+{
+    /*
+    class MainApp
+    {
+        private static void PrintThreadState(ThreadState state)
+        {
+            Console.WriteLine("{0, -16} : {1}", state, (int)state);
+        }
+        static void Main(string[] args)
+        {
+            PrintThreadState(ThreadState.Running);
+
+            PrintThreadState(ThreadState.StopRequested);
+
+            PrintThreadState(ThreadState.SuspendRequested);
+
+            PrintThreadState(ThreadState.Background);
+
+            PrintThreadState(ThreadState.Unstarted);
+
+            PrintThreadState(ThreadState.Stopped);
+
+            PrintThreadState(ThreadState.WaitSleepJoin);
+
+            PrintThreadState(ThreadState.Suspended);
+
+            PrintThreadState(ThreadState.Aborted);
+
+            PrintThreadState(ThreadState.Aborted | ThreadState.Stopped);
+        }
+
+
+    }
+    */ 
+}
